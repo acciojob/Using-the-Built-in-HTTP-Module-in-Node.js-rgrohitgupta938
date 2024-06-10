9ea9639270639db1e16be4e8f7460d6100904ab4
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-06-10 23:55:13 +0530

    sec

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const path = require("path");
 const filePath = process.argv[2];
 
 if (!filePath) {
-  console.error("Please provide a file path as a command-line argument.");
   process.exit(1);
 }
 
@@ -14,11 +13,11 @@ const absolutePath = path.resolve(filePath);
 const server = http.createServer((req, res) => {
   fs.readFile(absolutePath, "utf8", (err, data) => {
     if (err) {
-      res.writeHead(500, { "Content-Type": "text/plain" });
-      res.end("Error: Could not read the file.");
+      res.write(err.toString());
+      res.end();
     } else {
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end(data);
+      res.write(data);
+      res.end();
     }
   });
 });
@@ -29,9 +28,3 @@ server.listen(PORT, () => {
 });
 
 module.exports = server;
-
-// TODO: Create an HTTP server
-
-// TODO: Listen on port 3000
-
-// DO NOT EDIT BELOW THIS LINE (Uncomment it once you are done with your code)
