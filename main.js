c62531bf81a69426935352662d38dac9b04a0bfe
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-06-10 20:52:56 +0530

    First mesg

--- main.js
+++ main.js
@@ -1,8 +1,36 @@
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const filePath = process.argv[2];
+
+if (!filePath) {
+  console.error("Please provide a file path as a command-line argument.");
+  process.exit(1);
+}
+
+const absolutePath = path.resolve(filePath);
+
+const server = http.createServer((req, res) => {
+  fs.readFile(absolutePath, "utf8", (err, data) => {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      console.log("Error: Could not read the file.");
+    } else {
+      console.log("Hello, World!");
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end(data);
+    }
+  });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
 
 // TODO: Create an HTTP server
 
 // TODO: Listen on port 3000
 
 // DO NOT EDIT BELOW THIS LINE (Uncomment it once you are done with your code)
-
